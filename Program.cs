6e14499b1961084b9c2a076e845f8fabc4bba2eb
nick <yannick.katambo@outlook.com>
2020-11-08 23:40:55 +0200

    added connection string access

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace SampleAppOne
 {
@@ -10,6 +12,9 @@ namespace SampleAppOne
     {
         static void Main(string[] args)
         {
+            ConnectionStringSettings cnnString = ConfigurationManager.ConnectionStrings["DefaultConnection"];
+            WriteLine($"Connection String: {cnnString}");
+            ReadKey();
         }
     }
 }
